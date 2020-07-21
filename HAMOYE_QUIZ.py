e75f6299cbf4ca8b3e5e71eb04bb7edb6a0ed5bc
ummu-anas <67806767+ummu-anas@users.noreply.github.com>
2020-07-21 17:35:07 -0400

    Update HAMOYE_QUIZ.py

--- HAMOYE_QUIZ.py
+++ HAMOYE_QUIZ.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import pandas as pd
+import numpy as np
+
 url= 'https://raw.githubusercontent.com/WalePhenomenon/climate_change/master/fuel_ferc1.csv'
 US_fuel_data= pd.read_csv(url, error_bad_lines=False) 
 
 
-# In[2]:
-
-
 US_fuel_data.describe(include='all')
-
-
-# In[3]:
-
 
 # Question 1
 A = [1,2,3,4,5,6]
@@ -24,130 +16,44 @@ B = [13, 21, 34]
 A_B = A.extend(B)
 print(A_B)
 
-
-# In[4]:
-
-
 #Question 2
-import numpy as np
-import pandas as pd
-
-
 
 np.identity(3)
-
-
-# In[ ]:
-
-
-
-
-
-# In[5]:
-
 
 np.array([[1., 0., 0.],
        [0., 1., 0.],
        [0., 0., 1.]])
 
-
-# In[6]:
-
-
 #question 3
 US_fuel_data.groupby('fuel_type_code_pudl').mean()
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[7]:
-
-
-#question 6
-US_fuel_data.isnull().sum()
-
-
-# In[ ]:
-
-
-
-
-
-# In[8]:
-
-
-US_fuel_data.dropna()
-
-
-# In[ ]:
-
-
-
-
-
-# In[9]:
-
-
-#question 4
+#question 4a
 US_fuel_data.dropna().loc[:,'fuel_mmbtu_per_unit'].std()
 
 
-# In[10]:
-
-
-#question 4
+#question 4b
 US_fuel_data.dropna().loc[:,'fuel_mmbtu_per_unit'].quantile(0.75)
-
-
-# In[11]:
-
 
 #question 5
 US_fuel_data.loc[:,'fuel_qty_burned'].kurtosis()
 
-
-# In[12]:
-
-
 US_fuel_data.loc[:,'fuel_qty_burned'].skew()
 
+#question 6
+US_fuel_data.isnull().sum()
 
-# In[13]:
-
-
-#question10
-US_fuel_data.groupby('report_year')['fuel_cost_per_unit_delivered'].mean()
-
-
-# In[36]:
-
+US_fuel_data.dropna()
 
 #question 9
 US_fuel_data.groupby(['fuel_type_code_pudl', 'report_year'])[ 'fuel_qty_burned'].sum()
-
-
-# In[39]:
-
 
 # question 9
 perc_change = (8.98 - 7.17)/8.98
 print(perc_change)
 
-
-# 
-# 
-
-# In[ ]:
+#question10
+US_fuel_data.groupby('report_year')['fuel_cost_per_unit_delivered'].mean()
 
 
 
